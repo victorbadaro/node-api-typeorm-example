b3ea954b35617bf1646e3807d7f035a95237f95d
Victor <victor.badaro@fatec.sp.gov.br>
2021-12-27 18:08:54 -0300

    feat: add config to receive requests with json content and import routes file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import 'dotenv/config';
 import 'reflect-metadata';
 import express from 'express';
+import { router } from './routes';
 import './database';
 
 const app = express();
 const SERVER_PORT = process.env.SERVER_PORT;
+
+app.use(express.json());
+app.use(router);
 
 app.listen(SERVER_PORT, () => console.log(`Server is running! Port: ${SERVER_PORT}`));
