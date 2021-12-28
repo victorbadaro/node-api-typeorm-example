32f7094e04eb99953bf7c5f2f95a0a5dd8041d6f
Victor <victor.badaro@fatec.sp.gov.br>
2021-12-27 23:55:24 -0300

    feat: add methods to find, create, update and delete users

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,31 +1,68 @@
 import { Request, Response } from 'express';
+import { getRepository, Not } from 'typeorm';
+import { User } from '../entities/User';
 
 class UserController {
     async index(request: Request, response: Response) {
-        return response.json();
+        const repository = getRepository(User);
+        const users = await repository.find();
+
+        return response.json(users);
     }
 
     async find(request: Request, response: Response) {
         const { id } = request.params;
+        const repository = getRepository(User);
+        const user = await repository.findOne(id);
 
-        return response.json();
+        if(!user)
+            return response.status(400).json({ error: 'User not found!' });
+
+        return response.json(user);
     }
 
     async create(request: Request, response: Response) {
         const { name, email } = request.body;
+        const repository = getRepository(User);
 
-        return response.status(201).json();
+        if(await repository.findOne({ where: {email} }))
+            return response.status(400).json({ error: 'Email already exists!' });
+        
+        const user = repository.create({ name, email });
+
+        await repository.save(user);
+
+        return response.status(201).json(user);
     }
 
     async update(request: Request, response: Response) {
         const { id } = request.params;
         const { name, email } = request.body;
+        const repository = getRepository(User);
+        const user = await repository.findOne(id);
+
+        if(!user)
+            return response.status(400).json({ error: 'User not found!' });
+        
+        if(await repository.findOne({ where: { email, id: Not(id) } }))
+            return response.status(400).json({ error: 'Email already exists!' });
+        
+        user.name = name ? name : user.name;
+        user.email = email ? email : user.email;
+
+        await repository.save(user);
 
         return response.status(204).send();
     }
 
     async delete(request: Request, response: Response) {
         const { id } = request.params;
+        const repository = getRepository(User);
+
+        if(!await repository.findOne(id))
+            return response.status(400).json({ error: 'User not found!' });
+        
+        await repository.delete(id);
 
         return response.status(204).send();
     }
